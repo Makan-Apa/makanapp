88c6e077dd782139721d19daf4833fe24016f73f
ahmadalhq <ahmadalhq@gmail.com>
2020-06-11 23:10:13 +0700

    add controller for zomato and weather apis

--- server/router/index.js
+++ server/router/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRouter = require('./user.js');
+const dataRouter = require('./data.js');
 
 router.get('/', () => {
   console.log('Welcome to MakanApp')
 });
 router.use('/', userRouter);
+router.use('/data', dataRouter);
 
 module.exports = router;
